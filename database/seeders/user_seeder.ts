394f402f1d9c898415f6d93c1dc3d582da9c1ad1
gustavo <gustavo.limafgz@gmail.com>
2026-03-10 09:56:57 -0300

    fix: corrige duplo hash no seeder e atualiza README com comando docker test

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -1,14 +1,15 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import User from '#models/user'
-import hash from '@adonisjs/core/services/hash'
 
 export default class UserSeeder extends BaseSeeder {
     async run() {
+        // O withAuthFinder mixin faz o hash automaticamente no save()
+        // NÃO usar hash.make() aqui, pois causaria duplo hash
         await User.updateOrCreateMany('email', [
             {
                 name: 'Admin',
                 email: 'admin@admin.com',
-                password: await hash.make('Admin@123'),
+                password: 'Admin@123',
                 role: 'ADMIN',
             },
         ])
